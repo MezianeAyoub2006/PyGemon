7d5498fd2a989b9db95c8f259578fa354c0fc9e9
Silvereden <gokuu2202@gmail.com>
2023-11-17 21:21:40 +0100

    ayoub

--- scripts/scenes.py
+++ scripts/scenes.py
@@ -25,5 +25,20 @@ SCENES = [
         "invisible_layers" : ["inv"],
         "physical_layers" : ["physic", "inv"],
         "layers_z" : {"upper" : 3, "physic" : 1, "background" : 0, "inv" : -10, '_': 0.3} 
+    },
+
+    {
+        "scene_name" : "test_",
+        "world_limits" : ["left", "right", "up", "down"],
+        "tile_size" : 32,
+        "player" : 0,
+        "up_transition" : {"scene" : "test"},
+        "down_transition" : {"scene" : "test"},
+        "background_color" : [0, 0, 0],
+        "map_filepath" : "data/maps/test.json",
+        "tileset" : "main_tileset",
+        "invisible_layers" : ["inv"],
+        "physical_layers" : ["physic", "inv"],
+        "layers_z" : {"upper" : 3, "physic" : 1, "background" : 0, "inv" : -10, '_': 0.3} 
     }
 ]
